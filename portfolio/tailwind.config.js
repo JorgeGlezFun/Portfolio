6e3748265218c1746e80a803d3d1d227544509e5
jorgeglezfun <jorgegonfue4d@gmail.com>
2025-08-01 12:03:20 +0200

    Cambios al header (sin terminar) y adicion del menu de hamburguesa

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,15 +1,14 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+const defaultTheme = require('tailwindcss/defaultTheme');
+const forms = require('@tailwindcss/forms');
+const textShadow = require('@designbycode/tailwindcss-text-shadow');
 
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.jsx',
     ],
-
     theme: {
         extend: {
             fontFamily: {
@@ -17,6 +16,16 @@ export default {
             },
         },
     },
-
-    plugins: [forms],
+    plugins: [
+        forms,
+        require("@designbycode/tailwindcss-text-shadow"
+        )
+        ({
+            shadowColor: "rgba(0, 0, 0, 0.5)",
+            shadowBlur: "3px",
+            shadowOffsetX: "2px",
+            shadowOffsetY: "2px",
+            experimental: true
+        })
+    ],
 };
