aab249527c223cb1d2cf44ec33e9fde3b6c9e47e
JorgeGlezFun <jorge.gonzalez.fuentes.dev@gmail.com>
2025-12-25 21:16:16 +0100

    Cambios

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,16 +6,7 @@ use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-class UserSeeder extends Seeder
-{
-    public function run()
-    {
-        if (User::count() === 0) {
-            User::create([
-                'name' => 'Admin',
-                'email' => 'jorge.gonzalez.fuentes.dev@gmail.com',
-                'password' => Hash::make('10.de.Mayo.del.2000'),
-            ]);
-        }
-    }
-}
+// class UserSeeder extends Seeder
+// {
+
+// }
